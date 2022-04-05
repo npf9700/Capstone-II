a7da44dd9113dc49f4343bc101c7d0cec2cc55b5
BenGoldfeder <goldfedb@gmail.com>
2022-04-05 04:41:21 -0400

    Wiimote Functional
    If a wiimote is detected, a pointer will appear onscreen

--- Assets/Scripts/PointerManager.cs
+++ Assets/Scripts/PointerManager.cs
@@ -12,18 +12,29 @@ public class PointerManager : MonoBehaviour
     private Quaternion initial_rotation;
     private Wiimote wiimote;
     public GameObject Pointer_P1;
+    //public float oldPos;
+    public Camera cam;
+    private float cameraHeight;
+    private float cameraWidth;
+    public Vector2 NewPosition;
 
-    //public Color pointRed = red;
-    //public Color pointBlue = blue;
+
+    public Color pointRed = Color.red;
+    public Color pointBlue = Color.blue;
     //public Color pointGreen = green;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        // Instantiate at position (0, 0) and zero rotation.
-        Instantiate(Pointer_P1, new Vector2(0, 0), Quaternion.identity);
+        cameraHeight = cam.orthographicSize * 2f;
+        cameraWidth = cameraHeight * cam.aspect;
 
+        // Instantiate at position (0, 0) and zero rotation.
+        //GameObject myPointer = Instantiate(Pointer_P1, new Vector2(0, 0), Quaternion.identity);
+        WiimoteManager.FindWiimotes();
+        wiimote = WiimoteManager.Wiimotes[0];
+        wiimote.SetupIRCamera(IRDataType.BASIC);
     }
 
 
@@ -34,31 +45,52 @@ public class PointerManager : MonoBehaviour
         if (!WiimoteManager.HasWiimote()) { return; }
 
         wiimote = WiimoteManager.Wiimotes[0];
-
+        
         int ret;
         do
         {
             ret = wiimote.ReadWiimoteData();
         } while (ret > 0);
 
+
         //pointer[0] is x position, pointer[1] is y position
         float[] pointer = wiimote.Ir.GetPointingPosition();
-        Pointer_P1.transform.position = new Vector2(pointer[0], pointer[1]);
+
+        /**
+        if (pointer[0] != oldPos)
+        {
+            Debug.Log("Changed!");
+        }
+        **/
+
+        //Pointer_P1.transform.position = new Vector2( 0+Mathf.Round(pointer[0]*cameraWidth), 0+Mathf.Round(pointer[1]*cameraHeight) );
+        //Instantiate(Pointer_P1, new Vector2((Mathf.Round(pointer[0] * cameraWidth) - cameraWidth/2) , (Mathf.Round(pointer[1] * cameraHeight)-cameraHeight/2) ), Quaternion.identity);
+        //Instantiate(Pointer_P1, new Vector2( (pointer[0]*12)-6, (pointer[1]*12)-6 ), Quaternion.identity);
+
+        NewPosition = new Vector2 ( (pointer[0] * 10) + (cameraWidth/4), (pointer[1] * 10) - (cameraHeight) ) ;
+
+        Pointer_P1.transform.position = NewPosition;
+
+        //Instantiate(Pointer_P1, NewPosition, Quaternion.identity);
+
 
         //if "A" pressed, set hue to ... . if "A and B" pressed, set hue to ... . Else set hue to normal
         if (wiimote.Button.a == true) {
 
             if (wiimote.Button.b == true)
             {
-                Pointer_P1.GetComponent<Renderer>().material.color = Color.green;
+                //Pointer_P1.GetComponent<Renderer>().material.color = Color.green;
+                Debug.Log("Trash Move Input!");
             }
             else
             {
-                Pointer_P1.GetComponent<Renderer>().material.color = Color.blue;
+                //Pointer_P1.GetComponent<Renderer>().material.color = pointRed;
+                Debug.Log("Bubble Pop Input!");
             }
 
         } else {
-            Pointer_P1.GetComponent<Renderer>().material.color = Color.red;
+            //Pointer_P1.GetComponent<SpriteRenderer>().color = pointRed;
+            Debug.Log("No Input!");
         }
 
         /**
