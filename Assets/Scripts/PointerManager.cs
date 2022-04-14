5a31dd7ab44f3f84a2bb2cad28c1ff45879f4c60
BenGoldfeder <goldfedb@gmail.com>
2022-04-14 00:35:55 -0400

    Wiimote Polished + Mouse Input
    Heavily reduces wiimote jitter. Adds ability to use mouse if no wiimote is found.

--- Assets/Scripts/PointerManager.cs
+++ Assets/Scripts/PointerManager.cs
@@ -15,33 +15,53 @@ public class PointerManager : MonoBehaviour
     private float yOffset;
 
     private Quaternion initial_rotation;
-    private Wiimote wiimote;
-    public GameObject Pointer_P1;
+
+    private Wiimote wiimote1;
+    public GameObject P1_IR;
+    public Vector2 P1_IRPosition;
+    public GameObject P1_Cursor;
+    public Vector2 P1_CursorPosition;
+    public float moveSpeed = 10f;
+
+    private Wiimote wiimote2;
+    private GameObject P2_IRLocation;
+    public GameObject P2_CursorLocation;
+
     public GameObject myPrefab;
-    public Vector2 pointerPosition;
+    public bool usingMouse;
 
 
 
     // Start is called before the first frame update
     void Start()
     {
-        //Setting up camera values
+        // Setting up camera values
         cameraHeight = cam.orthographicSize * 2f;
         cameraWidth = cameraHeight * cam.aspect;
         xOffset = cameraWidth / 2;
         yOffset = cameraHeight / 2;
         Debug.Log("camera width and height: " + cameraWidth + ", " + cameraHeight);
 
-        //Bounds Calibration
+        // Bounds Calibration, shows current corners of camera and wiimote bounds
         Instantiate(myPrefab, new Vector2((0f * cameraWidth) - xOffset, (1f  *cameraHeight) - yOffset), Quaternion.identity);
         Instantiate(myPrefab, new Vector2((0f * cameraWidth) - xOffset, (0f * cameraHeight) - yOffset), Quaternion.identity);
         Instantiate(myPrefab, new Vector2((1f * cameraWidth) - xOffset, (1f * cameraHeight) - yOffset), Quaternion.identity);
         Instantiate(myPrefab, new Vector2((1f * cameraWidth) - xOffset, (0f * cameraHeight) - yOffset), Quaternion.identity);
 
-        //Setting up Wiimote
+        // Setting up Wiimote. Uses mouse location if none are found.
         WiimoteManager.FindWiimotes();
-        wiimote = WiimoteManager.Wiimotes[0];
-        wiimote.SetupIRCamera(IRDataType.BASIC);
+        if (WiimoteManager.Wiimotes.Count != 0)
+        {
+            Debug.Log("Wiimote found!");
+            usingMouse = false;
+            wiimote1 = WiimoteManager.Wiimotes[0];
+            wiimote1.SetupIRCamera(IRDataType.BASIC);
+        } 
+        else 
+        {
+            Debug.Log("No Wiimotes found.");
+            usingMouse = true;
+        }
     }
 
 
@@ -49,36 +69,49 @@ public class PointerManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Reading Wiimote data
-        if (!WiimoteManager.HasWiimote()) { return; }
-        wiimote = WiimoteManager.Wiimotes[0];
-        int ret;
-        do
+
+        ////Reading Wiimote data
+        //if (!WiimoteManager.HasWiimote()) { return; }
+        //wiimote1 = WiimoteManager.Wiimotes[0];
+        //int ret;
+        //do
+        //{
+        //    ret = wiimote1.ReadWiimoteData();
+        //} while (ret > 0);
+
+        if (usingMouse == false)
         {
-            ret = wiimote.ReadWiimoteData();
-        } while (ret > 0);
-
-        //Getting pointer position: pointer[0] is x position, pointer[1] is y position
-        float[] pointer = wiimote.Ir.GetPointingPosition();
-
-        //Sets position of pointer to IR pointer location. The float is multiplied across the width/height of the camera. The additional numbers are offsets to center it.
-        pointerPosition = new Vector2 ( (pointer[0] * cameraWidth) - xOffset, (pointer[1] * cameraHeight) - yOffset);
-        Pointer_P1.transform.position = pointerPosition;
-
-        //Handles button presses
-        if (wiimote.Button.a == true) {
-            if (wiimote.Button.b == true)
-            {
-                //Call to Move Trash
-                //Check Accelerometer?
-            }
-            else
-            {
-                //Call to Pop Bubble
-            }
-        } else {
-            //Nothing
+            // Sets IR position: pointer[0] is x, pointer[1] is y
+            float[] pointer = wiimote1.Ir.GetPointingPosition();
+            P1_IRPosition = new Vector2((pointer[0] * cameraWidth) - xOffset, (pointer[1] * cameraHeight) - yOffset);
+        } 
+        else if (usingMouse == true) 
+        {
+            // Gets mouse and instead uses mouse position to guide the cursor
+            Vector3 mousePos = Input.mousePosition;
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+            P1_IRPosition = new Vector2(mousePos.x, mousePos.y);
         }
+
+        // Final movement of IR pointer and cursor
+        P1_IR.transform.position = P1_IRPosition;
+        P1_CursorPosition = Vector2.Lerp(P1_CursorPosition, P1_IRPosition, Time.deltaTime * moveSpeed);
+        P1_Cursor.transform.position = P1_CursorPosition;
+
+        ////Handles button presses
+        //if (wiimote1.Button.a == true) {
+        //    if (wiimote1.Button.b == true)
+        //    {
+        //        //Call to Move Trash
+        //        //Check Accelerometer?
+        //    }
+        //    else
+        //    {
+        //        //Call to Pop Bubble
+        //    }
+        //} else {
+        //    //Nothing
+        //}
     }
 
 }
